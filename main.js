76b8d39bf0275ff0abad1d6dd5163a08e6a4fbf0
Yevhen Shcherbynskyi <Yevhen.Shcherbynskyi@isobar.com>
2019-08-30 12:55:59 +0200

    Fix bug with auto update

--- main.js
+++ main.js
@@ -3,32 +3,29 @@ const { app, Menu, Tray, dialog } = require("electron");
 const { spawn } = require("child_process");
 const fixPath = require("fix-path");
 const fs = require("fs");
-require('update-electron-app')()
 const Store = require("electron-store");
 const Sentry = require("@sentry/electron");
-
 const AutoLaunch = require("auto-launch");
-
 const ElectronSampleAppLauncher = new AutoLaunch({
-    name: "Electron-sample-app"
+  name: "Electron-sample-app",
 });
-
+require("update-electron-app")();
 fixPath();
 
 Sentry.init({
-    dsn: "https://f7b5c91953ec4f1f980130a842840576@sentry.io/1533800"
+  dsn: "https://1b620b81c44a4520ad5b1138aed08ad6@sentry.io/1546533",
 });
 
 const schema = {
-    projects: {
-        type: "string"
-    }
+  pointvc: {
+    type: "string",
+  },
 };
 
 let mainTray = {};
 
 if (app.dock) {
-    app.dock.hide();
+  app.dock.hide();
 }
 
 const store = new Store({ schema });
@@ -37,108 +34,110 @@ const store = new Store({ schema });
   Language on computer
 */
 function getLocale() {
-    const locale = app.getLocale();
+  const locale = app.getLocale();
 
-    switch (locale) {
-        case "es-419" || "es":
-            return JSON.parse(fs.readFileSync(resolve(__dirname, "locale/es.json")));
-        case "pt-BR" || "pt-PT":
-            return JSON.parse(fs.readFileSync(resolve(__dirname, "locale/pt.json")));
-        default:
-            return JSON.parse(fs.readFileSync(resolve(__dirname, "locale/en.json")));
-    }
+  switch (locale) {
+    case "es-419" || "es":
+      return JSON.parse(fs.readFileSync(resolve(__dirname, "locale/es.json")));
+    case "pt-BR" || "pt-PT":
+      return JSON.parse(fs.readFileSync(resolve(__dirname, "locale/pt.json")));
+    default:
+      return JSON.parse(fs.readFileSync(resolve(__dirname, "locale/en.json")));
+  }
 }
 
 function render(tray = mainTray) {
-    const storedProjects = store.get("projects");
-    const projects = storedProjects ? JSON.parse(storedProjects) : [];
-    const locale = getLocale();
+  const storedProjects = store.get("projects");
+  const projects = storedProjects ? JSON.parse(storedProjects) : [];
+  const locale = getLocale();
 
-    const items = projects.map(({ name, path }) => ({
-        label: name,
-        submenu: [{
-                label: locale.open,
-                click: () => {
-                    spawn("code", [path], { shell: true });
-                }
-            },
+  const items = projects.map(({ name, path }) => ({
+    label: name,
+    submenu: [
+      {
+        label: locale.open,
+        click: () => {
+          spawn("code", [path], { shell: true });
+        },
+      },
+      {
+        label: locale.pull,
+        click: () => {
+          spawn(`cd ${path} && git pull`, { shell: true });
+        },
+      },
+      {
+        label: locale.remove,
+        click: () => {
+          store.set(
+            "projects",
+            JSON.stringify(projects.filter(item => item.path !== path))
+          );
+          render();
+        },
+      },
+    ],
+  }));
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: locale.add,
+      click: () => {
+        const result = dialog.showOpenDialog({ properties: ["openDirectory"] });
+
+        if (!result) return;
+
+        const [path] = result;
+        const name = basename(path);
+
+        store.set(
+          "projects",
+          JSON.stringify([
+            ...projects,
             {
-                label: locale.pull,
-                click: () => {
-                    spawn(`cd ${path} && git pull`, { shell: true });
-                }
+              path,
+              name,
             },
-            {
-                label: locale.remove,
-                click: () => {
-                    store.set(
-                        "projects",
-                        JSON.stringify(projects.filter(item => item.path !== path))
-                    );
-                    render();
-                }
-            }
-        ]
-    }));
+          ])
+        );
 
-    const contextMenu = Menu.buildFromTemplate([{
-            label: locale.add,
-            click: () => {
-                const result = dialog.showOpenDialog({ properties: ["openDirectory"] });
+        render();
+      },
+    },
+    {
+      type: "separator",
+    },
+    ...items,
+    {
+      type: "separator",
+    },
+    {
+      type: "normal",
+      label: locale.close,
+      role: "quit",
+      enabled: true,
+    },
+  ]);
 
-                if (!result) return;
+  tray.setContextMenu(contextMenu);
 
-                const [path] = result;
-                const name = basename(path);
-
-                store.set(
-                    "projects",
-                    JSON.stringify([
-                        ...projects,
-                        {
-                            path,
-                            name
-                        }
-                    ])
-                );
-
-                render();
-            }
-        },
-        {
-            type: "separator"
-        },
-        ...items,
-        {
-            type: "separator"
-        },
-        {
-            type: "normal",
-            label: locale.close,
-            role: "quit",
-            enabled: true
-        }
-    ]);
-
-    tray.setContextMenu(contextMenu);
-
-    tray.on("click", tray.popUpContextMenu);
+  tray.on("click", tray.popUpContextMenu);
 }
 
 app.on("ready", () => {
-    mainTray = new Tray(resolve(__dirname, "assets", "iconTemplate.png"));
+  mainTray = new Tray(resolve(__dirname, "assets", "iconTemplate.png"));
 
-    render(mainTray);
-    ElectronSampleAppLauncher.enable();
+  render(mainTray);
+  ElectronSampleAppLauncher.enable();
 
-    ElectronSampleAppLauncher.isEnabled()
-        .then(function(isEnabled) {
-            if (isEnabled) {
-                return;
-            }
-            ElectronSampleAppLauncher.enable();
-        })
-        .catch(function(err) {
-            // handle error
-        });
+  ElectronSampleAppLauncher.isEnabled()
+    .then(function(isEnabled) {
+      if (isEnabled) {
+        return;
+      }
+      ElectronSampleAppLauncher.enable();
+    })
+    .catch(function(err) {
+      // handle error
+    });
 });
